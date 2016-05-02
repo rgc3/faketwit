d59c6b628788d3e844fd90a96a94f25252f1ce00
Robert Cunningham <rgc3dev@gmail.com>
2016-05-02 15:19:12 -0400

    Add root. Create new tweet view.

--- faketwit/app/controllers/tweets_controller.rb
+++ faketwit/app/controllers/tweets_controller.rb
@@ -1,2 +1,16 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+
+  before_filter :authenticate_user!
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    respond_to do |format|
+      if @tweet.save
+        format.html { redirect_to @tweet, notice: "Tweet was successfully created." }
+      else
+        format.html { render :new }
+      end
+  end
 end
